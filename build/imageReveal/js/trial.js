6460632b88f852795e67aac44a33ced70b2433a7
Basilogast <duyhung08112003@gmail.com>
2024-09-04 04:43:38 +0700

    Modify vite.config

--- build/imageReveal/js/trial.js
+++
@@ -1,5 +0,0 @@
-alert("Hello world!");
-console.log("Hello world!");
-while (true) {
-    console.log("1");
-}
--- build/testScript.js
+++
@@ -1,7 +0,0 @@
-// public/testScript.js
-console.log("External script loaded successfully!");
-console.log("hi");
-// alert("Hello");
-// while (true) {
-//     console.log("1");
-// }
